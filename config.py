7c4b9ce9adf647fb837a96ab583559305b0e9c89
Nitheesh <nitheesh.me@yahoo.com>
2020-01-31 14:03:34 +0530

    Added Procfile

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 import os
-if os.environ["HEROKU"]:
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
+try:
+    if os.environ["HEROKU"]:
+        print("Heroku Platform")
+        SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+        SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
 
-    FLASK_ADMIN_SWATCH = os.environ['FLASK_ADMIN_SWATCH']
-    SECRET_KEY = os.environ['SECRET_KEY']
-else:
-    import config_vars
-    SQLALCHEMY_DATABASE_URI = config_vars.SQLALCHEMY_DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = config_vars.SQLALCHEMY_TRACK_MODIFICATIONS
+        FLASK_ADMIN_SWATCH = os.environ['FLASK_ADMIN_SWATCH']
+        SECRET_KEY = os.environ['SECRET_KEY']
+except KeyError:
+    try:
+        import config_vars
+        SQLALCHEMY_DATABASE_URI = config_vars.SQLALCHEMY_DATABASE_URI
+        SQLALCHEMY_TRACK_MODIFICATIONS = config_vars.SQLALCHEMY_TRACK_MODIFICATIONS
 
-    FLASK_ADMIN_SWATCH = config_vars.FLASK_ADMIN_SWATCH
-    SECRET_KEY = config_vars.SECRET_KEY
+        FLASK_ADMIN_SWATCH = config_vars.FLASK_ADMIN_SWATCH
+        SECRET_KEY = config_vars.SECRET_KEY
+    except:
+        print("Some Error")
