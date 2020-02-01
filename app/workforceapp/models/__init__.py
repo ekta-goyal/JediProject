510684118f6f479cbc0c288283e83ac1b981964e
Nitheesh <nitheesh.me@yahoo.com>
2020-02-01 11:44:19 +0530

    Rearrange of imports in Task and Task_status as Task is dependent on task status

--- app/workforceapp/models/__init__.py
+++ app/workforceapp/models/__init__.py
@@ -1,5 +1,7 @@
 from .user import User
 from .team import Team
 from .user_team_mapping import User_team_mapping
+from .task_status import Task_status
 from .task import Task
-
+from .task_index_mapping import Task_index_mapping
+from .attachments import Attachments
